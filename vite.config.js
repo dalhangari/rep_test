81f73942c5f76e505d02f3d8829d65bd27d4eb1e
Seungyeon <prosum87@gmail.com>
2026-06-26 16:09:26 +0900

    chore: add deploy configurations

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/rep_test/' // 깃허브 페이지 배포를 위한 서브디렉토리 base 경로 설정
 })
